3d6bd6f66fa4e181862be70d7a92b57339a7da1e
Blarc <jakob.malezic@gmail.com>
2025-07-29 19:51:16 +0200

    chore: remove unused MessageRepository

--- src/main/java/com/github/blarc/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.blarc.repository;
-
-import com.github.blarc.entity.Message;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class MessageRepository implements PanacheRepository<Message> {
-}
